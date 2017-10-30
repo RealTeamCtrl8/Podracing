4b038879db298bf269b84b1be7f422118372e90b
mirrorapps <eli.speigel@gmail.com>
2017-10-30 10:27:31 -0700

    updated testing example data

--- test/unit/vehicle.test.js
+++ test/unit/vehicle.test.js
@@ -2,7 +2,7 @@ const {assert} = require('chai');
 const Vehicle = require('../../lib/models/vehicle');
 
 describe('Vehicle model', () => {
-    const vehicle = new Vehicle({name: 'tawser'});
+    const vehicle = new Vehicle({name: 'tawser', cost_in_credits: '100000000', max_atmosphering_speed: '123123'});
 
     it('should validate model', () => {
         const validate = vehicle.validateSync();
