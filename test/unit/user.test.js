a3939e57c993e2386ad7a6f13297085a53db181a
ckrountree <jmail0207@gmail.com>
2017-10-30 11:50:09 -0700

    bad user info validation tests passing

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -1,26 +1,25 @@
 const {assert} = require('chai');
 const User = require('../../lib/models/user');
-// const Schema = mongoose.Schema;
-const RequiredString = require('../../lib/utils/required-string');
 
 describe( 'User Model', () => {
     const user = new User ({
-        name: RequiredString,
-        email: RequiredString,
-        password: RequiredString,
-        // character: {
-        //     type: Schema.Types.ObjectId,
-        //     ref: 'Character' 
-        // }    
+        name: 'ckr',
+        email: 'me@me.com',
+        password: 'hello',
+        character: '59eb914ffcd9df5be2d25d18'   
     });
 
     it('should validate model', () => {
-        assert.equal(user.validateSync(), 'undefined');
+        assert.equal(user.validateSync(), undefined);
     });
 
     it('checks required fields', () => {
         const badUser = new User ({});
         const {errors} = badUser.validateSync();
         assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.password.kind, 'required');
+        // TODO:  need .getAll method before validating this test
+        // assert.equal(errors.character.kind, '???');
     });
 });
