dd6027e7ce2a334d5f4e44ef21d35241bc94611b
Robin Miller <rmillerpdx@gmail.com>
2017-10-30 15:42:03 -0700

    fix port variable for real this time

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log('server is running', server.address().PORT);
+    console.log('server is running', server.address().port);
 });
