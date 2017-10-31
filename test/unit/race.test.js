a426ec21e2686c8ac755e1f0a36dd41caa5fe60f
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-31 14:46:44 -0700

    race model and unit test done

--- test/unit/race.test.js
+++ test/unit/race.test.js
@@ -2,26 +2,22 @@ const {assert} = require('chai');
 const Race = require('../../lib/models/race');
 
 
+
 describe('Race model', () => {
-    const race = new Race({
-        planet: 'Aleran',
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        endTime: Number
 
-
-    });
-
-    it.only('should validate a good model', () => {
+    it('should validate a good model', () => {
+        const race = new Race({
+            planet: '59ef87b38e62d836e1c0ee48',
+            user: '59ef87b38e62d836e1c0ee41',
+            endTime: new Date
+        });
         const validate = race.validateSync();
         assert.equal(validate, undefined);
     });
 
-    it.skip('', () => {
+    it('Checks required fields', () => {
         const badRace = new Race({}); 
         const { errors } = badRace.validateSync();
-        assert.equal(errors.race.kind,'required');
+        assert.equal(errors.planet.kind,'required');
     });
 });
