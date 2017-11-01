2829b5377f1c32fd35dc88077c60f3bd6b40b17f
Robin Miller <rmillerpdx@gmail.com>
2017-11-01 09:22:26 -0700

    fix dotnet bug

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const http = require('http');
 const app = require('./lib/app');
 const server = http.createServer(app);
@@ -7,7 +7,6 @@ const connect = require('./lib/connect');
 connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/starwars');
 
 // const bodyParser = require('body-parser');
-
 
 const port = process.env.PORT || 3000;
 
