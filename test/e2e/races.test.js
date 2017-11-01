978ffc9de7e2a2256ff3567befe35258acdeeba8
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-31 19:05:40 -0700

    working on rest methods for race

--- test/e2e/races.test.js
+++ test/e2e/races.test.js
@@ -4,33 +4,57 @@ const db = require('./db');
 
 describe('Races test', () => {
     beforeEach(() => db.drop());
+    let race = null;
+    let planet = null;
 
-    const hothCircuit = {
-        
+
+    const hoth = {
+        planet: 'hoth'
     };
 
-    it.only('Posts a race to the api', () => {
+    beforeEach(() =>{
+        return request.post('/api/starwars')
+            .send(hoth)
+            .then(res => planet = res.body);
+    });
 
+    beforeEach(() => {
+        race = {
+            planet: planet._id,
+            endTime: new Date
+        };
+        return request.post('/api/starwars')
+            .send(race)
+            .then(res => race = res.body);
+    });
+
+
+    it.only('Posts a race to the api', () => {
+        return request.post('/api/starwars')
+            .send(race)
+            .then(res => {
+
+            })
     });
 
     it.skip('should retrieve all Races from api', () => {
-        return request.get('/api/races')
+        return request.get('/api/starwars')
         //todo: rewrite differently
             .then(({ body }) => {
                 assert.ok(body[0].name);
             });
     });
 
-    it.skip('Checks the raceDetails function', () => {
-
+    it.skip('Checks the raceScheduler function', () => {
+        //todo: write raceScheduler
 
     });
 
 
 });
 
-return {
-    planetArray: this.planet,//todo get 1 random planet from the array
-    distance: distance,
-    prize: prize
-};
+// return {
+//     planetArray: this.planet,//todo get 1 random planet from the array
+//     distance: distance,
+//     prize: prize
+// };
