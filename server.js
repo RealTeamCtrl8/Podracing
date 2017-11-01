0f51521e27abb54fa9772062369a7c96b432ef93
Robin Miller <rmillerpdx@gmail.com>
2017-11-01 15:45:45 -0700

    move timer code into utils and create test to do once race schema is completed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const app = require('./lib/app');
 const server = http.createServer(app);
 const connect = require('./lib/connect');
+const scheduler = require('./lib/timers/scheduler');
 
 connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/starwars');
 
@@ -14,21 +15,6 @@ server.listen(port, () => {
     console.log('server is running', server.address().port);//eslint-disable-line
 });
 
-function checkPlaceholder(){
-    //check races in db collection for endTime < current date
-    console.log('checking for completed races');
-}
 
-function schedulePlaceholder(){
-    //Populate race db with new race instances
-    console.log('adding new races');
-}
-
-const checkRaces = setInterval(checkPlaceholder, 1000);
-const scheduleRaces = setInterval(schedulePlaceholder, 5000);
-
-process.on('SIGINT', () => {
-    console.log('switching off timers...');
-    clearInterval(checkRaces);
-    clearInterval(scheduleRaces);
-});
+//TODO seed data - get all planets/vehicles/characters
+scheduler();
