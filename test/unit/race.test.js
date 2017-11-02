32447a68425720500fc349747b906b97ec7b14e9
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-02 08:53:37 -0700

    added prize and winner to race schema

--- test/unit/race.test.js
+++ test/unit/race.test.js
@@ -8,9 +8,11 @@ describe('Race model', () => {
     it('should validate a good model', () => {
         const race = new Race({
             planet: '59ef87b38e62d836e1c0ee48',
-            user: '59ef87b38e62d836e1c0ee41',
+            user: ['59ef87b38e62d836e1c0ee41', '59ef87b38e62d836e1c0ff41'],
             endTime: new Date,
-            active: true
+            active: true,
+            prize: 1234,
+            winner: '59ef87b38e62d836e1c0ee41'
         });
         const validate = race.validateSync();
         assert.equal(validate, undefined);
@@ -21,5 +23,6 @@ describe('Race model', () => {
         const { errors } = badRace.validateSync();
         assert.equal(errors.planet.kind,'required');
         assert.equal(errors.active.kind,'required');
+        assert.equal(errors.prize.kind, 'required');
     });
 });
