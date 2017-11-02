09c80fa60656774e84e3dbc7b5f02b74e337c16d
ckrountree <jmail0207@gmail.com>
2017-11-02 11:01:42 -0700

    create test for create-race failing

--- test/e2e/create-race.test.js
+++ test/e2e/create-race.test.js
@@ -1,27 +1,31 @@
 const { assert } = require('chai');
 const db = require('./db');
-const schedule = require('../../lib/timers/create-race');
+const request = require('./request');
+// const schedule = require('../../lib/timers/create-race');
 const Race = require('../../lib/models/race');
 
-
-
-
-describe('Create race test', () => {
+describe.only('Create race test', () => {
 
     beforeEach(() => {
         db.drop();
-
-        
     });
 
-    //TODO: finish test once race schema is completed
+    const testRace = new Race({
+        planet: Alderaan,
+        users: '59ef87b38e62d836e1c0ee41',
+        endTime: new Date(11 - 2 - 2017),
+        active: true,
+        prize: 100,
+        winner: null
+    });
+
     it('should create races', () => {
-        return schedule()
-            .then( () => {
-                return Race.find().lean();
-            })
-            .then( races => {
-                assert
+        return request.post('/api/races')
+            .send(schedule(testRace))
+            .then(res => {
+                const race = res.body;
+                assert.ok(race._id);
+                assert.equal(race.users, testRace.users);
             });
 
     });
