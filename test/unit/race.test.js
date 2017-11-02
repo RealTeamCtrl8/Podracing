9357d662900804f4750eb80cc31749b5455d272d
ckrountree <jmail0207@gmail.com>
2017-11-01 20:00:50 -0700

    add boolean to active race model, tests passing

--- test/unit/race.test.js
+++ test/unit/race.test.js
@@ -3,13 +3,14 @@ const Race = require('../../lib/models/race');
 
 
 
-describe('Race model', () => {
+describe.only('Race model', () => {
 
     it('should validate a good model', () => {
         const race = new Race({
             planet: '59ef87b38e62d836e1c0ee48',
             user: '59ef87b38e62d836e1c0ee41',
-            endTime: new Date
+            endTime: new Date,
+            active: true
         });
         const validate = race.validateSync();
         assert.equal(validate, undefined);
@@ -19,5 +20,6 @@ describe('Race model', () => {
         const badRace = new Race({}); 
         const { errors } = badRace.validateSync();
         assert.equal(errors.planet.kind,'required');
+        assert.equal(errors.active.kind,'required');
     });
 });
