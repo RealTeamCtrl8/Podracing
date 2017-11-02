3ed9563d21ef5b4a473f13e973e618a6fbd4edf3
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-02 15:42:25 -0700

    drop the db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const app = require('./lib/app');
 const server = http.createServer(app);
 const connect = require('./lib/connect');
+const mongoose = require('mongoose');
 
 // const scheduler = require('./lib/scripts/scheduler');
 
@@ -15,7 +16,7 @@ connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/starwars');
 // const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-
+mongoose.connection.dropDatabase();
 seedPlanets();
 seedVehicles();
 seedCharacters();
